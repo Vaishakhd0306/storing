34e680023dbadf2877556c46395365cef65ff5e2
Vaish <vaishakhd0306@gmail.com>
2024-09-11 21:10:11 +0530

    Revert "first commit"
    This reverts commit e7a742592d939981789808cfc10eb8265e49c25e.

--- utils/format.ts
+++
@@ -1,9 +0,0 @@
-export const formatCurrency = (amount: number) => {
-    return new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-    }).format(amount);
-  };
-  
-
-  
